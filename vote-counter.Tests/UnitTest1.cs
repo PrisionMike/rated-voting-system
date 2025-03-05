0dc15d3482009d6fe9026775fd403a285e4ec2eb
Suyash Shandilya <su.sh2396@gmail.com>
2025-03-05 01:35:31 +0100

    Test proj added. Basic test written

---
+++ vote-counter.Tests/UnitTest1.cs
@@ -0,0 +1,27 @@
+﻿using Moq;
+using vote_counter.Controllers;
+using vote_counter.Data;
+using vote_counter.Models;
+
+namespace vote_counter.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public async Task one_vote_election()
+    {
+        var mockdbContext = new Mock<AppDbContext>();
+        var mockDbSet = new Mock<Vote>();
+        
+        mockdbContext.Setup(db => db.Votes).Returns(mockDbSet.Object);
+        var controller = new VoteCastingController(mockdbContext.Object);
+        var vote = new Vote {Candidate1 = 1, ElectionId = 1};
+        
+        await controller.voteCasting(vote);
+        var result = await controller.GetElectionResult(1);
+        
+        Assert.Equal(result, 1);
+        
+    }
+    
+}
