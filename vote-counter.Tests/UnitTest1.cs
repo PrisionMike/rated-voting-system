5513ced9c1ea92fcba6271378bb4302286985a49
Suyash Shandilya <su.sh2396@gmail.com>
2025-03-08 16:32:00 +0100

    accidentally removed entire repo

---
+++ vote-counter.Tests/UnitTest1.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Moq;
+using vote_counter.Controllers;
+using vote_counter.Data;
+using vote_counter.Models;
+
+namespace vote_counter.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public async Task one_vote_election()
+    {
+        var mockdbContext = new Mock<AppDbContext>();
+        var mockDbSet = new Mock<DbSet<Vote>>();
+        
+        mockdbContext.Setup(db => db.Votes).Returns(mockDbSet.Object);
+        
+        var controller = new VoteCastingController(mockdbContext.Object);
+        var vote = new Vote {Candidate1 = 1, ElectionId = 1};
+        
+        await controller.voteCasting(vote);
+        var result = await controller.GetElectionResult(1);
+        
+        Assert.Equal(result, 1);
+        
+    }
+    
+}
---
+++ vote-counter/Models/Voter.cs
@@ -0,0 +1,7 @@
+namespace vote_counter.Models;
+
+public class Voter
+{
+    public int VoterId { get; set; }
+    public string VoterName  { get; set; }
+}
